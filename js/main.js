ca22df7c7dd54f59ac58847c0d5ad902d8e7f314
Looi Xiang Wen <xiangwen94@gmail.com>
2025-09-10 17:48:42 +0800

    home page update

--- js/main.js
+++ js/main.js
@@ -2051,6 +2051,25 @@
 			});
 	}
 
+	if ($('.parallax-text-line-2').length > 0) {
+		gsap.set('.tp-project-textline-2', {
+			x: '0%'
+		});
+
+		gsap.timeline({
+			scrollTrigger: {
+				trigger: '.tp-project-textline-2',
+				start: '-1500 10%',
+				end: 'bottom 20%',
+				scrub: true,
+				invalidateOnRefresh: true
+			}
+		})
+			.to('.tp-project-textline-2', {
+				x: '-100%'
+			});
+	}
+
 	/* portfolio animation start */
 	if ($('.tp-award-2-area').length > 0) {
 
@@ -2589,72 +2608,7 @@
 		});
 	}
 
-	if ($('.cta-text').length > 0) {
-
-		let cta = gsap.timeline({
-			repeat: -1,
-			delay: 0.5,
-			scrollTrigger: {
-				trigger: '.cta-text',
-				start: 'bottom 100%-=50px'
-			}
-		});
-		gsap.set('.cta-text', {
-			opacity: 0
-		});
-		gsap.to('.cta-text', {
-			opacity: 1,
-			duration: 1,
-			ease: 'power1.out',
-			scrollTrigger: {
-				trigger: '.cta-text',
-				start: 'bottom 100%-=50px',
-				once: true
-			}
-		});
 	
-		let mySplitText = new SplitText(".cta-text", { type: "words,chars" });
-		let chars = mySplitText.chars;
-		let endGradient = chroma.scale(['#FFB55E', '#F25164', '#7F00D7', '#EC38BC', '#F25164']);
-		cta.to(chars, {
-			duration: 0.5,
-			scaleY: 0.6,
-			ease: "power1.out",
-			stagger: 0.04,
-			transformOrigin: 'center bottom'
-		});
-		cta.to(chars, {
-			yPercent: -20,
-			ease: "elastic",
-			stagger: 0.03,
-			duration: 0.8
-		}, 0.5);
-		cta.to(chars, {
-			scaleY: 1,
-			ease: "elastic.out",
-			stagger: 0.03,
-			duration: 1.5
-		}, 0.5);
-		cta.to(chars, {
-			color: (i, el, arr) => {
-				return endGradient(i / arr.length).hex();
-			},
-			ease: "power1.out",
-			stagger: 0.03,
-			duration: 0.3
-		}, 0.5);
-		cta.to(chars, {
-			yPercent: 0,
-			ease: "back",
-			stagger: 0.03,
-			duration: 0.8
-		}, 0.7);
-		cta.to(chars, {
-			color: '#fff',
-			duration: 1.4,
-			stagger: 0.05
-		});
-	}
 
 
 	if ($('.tp-char-animation').length > 0) {
